fdc3ac0d55557a2e06acd5cae1cfb94c064d9c0e
M4x <maxmurray1999@gmail.com>
2023-04-18 15:47:56 +0100

    Task 2

--- task-2/src/components/App/index.js
+++ task-2/src/components/App/index.js
@@ -2,20 +2,47 @@ import "./app.css";
 
 import Input from "../Input";
 import List from "../List";
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+  names: []
+}
+
+function namesReducer(state, action) {
+  switch(action.type){
+    case "ADD_NAME": 
+    return {
+      names: [...state.names, action.payload]
+    }
+    default: 
+    throw new Error()
+  }
+
+
+  // const updatedNames = [...names, addedName];
+  // setNames(updatedNames);
+
+}
+
+
 
 function App() {
-  const [names, setNames] = useState([]);
+  const [state, dispatch] = useReducer(namesReducer, initialState);
 
   function addName(addedName) {
-    const updatedNames = [...names, addedName];
-    setNames(updatedNames);
+
+    dispatch({
+      type: "ADD_NAME",
+      payload: addedName
+    }
+      )
+      console.log("ADD_NAME")
   }
 
   return (
     <main>
       <Input addItem={addName} />
-      <List items={names} />
+      <List items={state.names} />
     </main>
   );
 }
